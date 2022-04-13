e10056f9e9437c697324b875efdc9c4814cd5270
sgvn <sacha.gavino@hotmail.fr>
2022-04-13 11:35:28 +0200

    update parameters name

--- chemdiskpy/modeling/Envelope.py
+++ chemdiskpy/modeling/Envelope.py
@@ -27,7 +27,7 @@ import parameters as p
 #  |_________| |__|  \___|     \____/
 #___________________________________________
 class Envelope:
-    def __init__(self, rmin=p.rin, rmax=p.rmax, r_centri=p.r_centri, \
+    def __init__(self, rmin=p.rmin, rmax=p.rmax, r_centri=p.r_centri, \
                        acc_rate=p.acc_rate, star_mass=p.star_mass, env_mass=p.env_mass, \
                        cavpl=p.cavpl, cav_fact=p.cav_fact, cavz0=p.cavz0):
         self.rmin=rmin
@@ -81,62 +81,62 @@ be returned, a `matrix` object is returned instead.
 #  |  |______  |  | \    |    \  \/  /
 #  |_________| |__|  \___|     \____/
 #_______________________________________________
-class Envelope:
-    def __init__(self, dist, mu, R_centri=p.R_centri, acc_rate=p.acc_rate, star_mass=p.star_mass):
-        self.R_centri=R_centri
-        self.acc_rate=acc_rate
-        self.star_mass=star_mass
+# class Envelope:
+#     def __init__(self, dist, mu, R_centri=p.R_centri, acc_rate=p.acc_rate, star_mass=p.star_mass):
+#         self.R_centri=R_centri
+#         self.acc_rate=acc_rate
+#         self.star_mass=star_mass
 
 
 
-    """
-	The following methods give the physical parameters related to the gas phase in the envelope.
-	The gas is given by Hydrogren nuclei. Equation references are related to the documentation.
-	Instance methods:
-        A) solution (parameter for the analytical solution)
-        B) mu_0()
-        C) env_density(self, acc_rate, star_mass, dist, R_centri, mu, mu_0)
-    """
+#     """
+# 	The following methods give the physical parameters related to the gas phase in the envelope.
+# 	The gas is given by Hydrogren nuclei. Equation references are related to the documentation.
+# 	Instance methods:
+#         A) solution (parameter for the analytical solution)
+#         B) mu_0()
+#         C) env_density(self, acc_rate, star_mass, dist, R_centri, mu, mu_0)
+#     """
 
-    """ A)
-    Equation (1). Initializer. Returns the parameter for the solution of the initial streamline of the infalling material 
-    Arguments:
-        -mu:            cos(theta) where theta is the angle above the midplane 
-        -dist:          distance to the central core in the spherical coordinates (dist, theta, phi) [au]
-        -R_centri:      centrifugal radius [au]
-    """
-    def solution(self, dist, mu, R_centri):
-        sol = (27*mu*dist*R_centri**2+np.sqrt(729*mu**2*dist**2*R_centri**4+108*R_centri**3*(dist-R_centri)**3))**(1/3)/(3*R_centri*2**(1/3))
-        #solution = 729*mu**2*dist**2*R_centri**4 + 108*R_centri**3*(dist-R_centri)**3
-        return sol
+#     """ A)
+#     Equation (1). Initializer. Returns the parameter for the solution of the initial streamline of the infalling material 
+#     Arguments:
+#         -mu:            cos(theta) where theta is the angle above the midplane 
+#         -dist:          distance to the central core in the spherical coordinates (dist, theta, phi) [au]
+#         -R_centri:      centrifugal radius [au]
+#     """
+#     def solution(self, dist, mu, R_centri):
+#         sol = (27*mu*dist*R_centri**2+np.sqrt(729*mu**2*dist**2*R_centri**4+108*R_centri**3*(dist-R_centri)**3))**(1/3)/(3*R_centri*2**(1/3))
+#         #solution = 729*mu**2*dist**2*R_centri**4 + 108*R_centri**3*(dist-R_centri)**3
+#         return sol
 
-    """ B)
-    Equation (2). Returns the solution of the initial streamline of the infalling material (cos(theta_0))
-    Arguments:
-        -dist:          distance to the central core in the spherical coordinates (dist, theta, phi) [au]
-        -R_centri:      centrifugal radius [au]
-    """	
-    def mu_0(self, dist, R_centri):
-        solution = self.solution()
-        return solution - ((1)/(3*solution))
+#     """ B)
+#     Equation (2). Returns the solution of the initial streamline of the infalling material (cos(theta_0))
+#     Arguments:
+#         -dist:          distance to the central core in the spherical coordinates (dist, theta, phi) [au]
+#         -R_centri:      centrifugal radius [au]
+#     """	
+#     def mu_0(self, dist, R_centri):
+#         solution = self.solution()
+#         return solution - ((1)/(3*solution))
 
-    """ C)
-    Equation (2). Returns the density of gas of the envelope [g.cm-3].
-    Arguments:
-        -acc_rate:      accretion rate in solar mass per year (converted in g per second)
-        -dist:          distance to the central core in the spherical coordinates (dist, theta, phi) [au]
-        -R_centri:      centrifugal radius [au]
-        -mu:            cos(theta) where theta is the angle above the midplane
-        -mu0:           initial streamline of the infalling material
-    """	
-    def env_density(self, acc_rate, star_mass, dist, R_centri, mu, mu_0):
-        return (((acc_rate*M_sun)/3.154e7)/(4*np.pi))*(Ggram*star_mass*M_sun*(dist*autocm)**3)**(-1/2)*(1 + mu/mu_0)**(-1/2)*(mu/mu_0 + 2*mu_0**2*(R_centri/dist))**(-1)
+#     """ C)
+#     Equation (2). Returns the density of gas of the envelope [g.cm-3].
+#     Arguments:
+#         -acc_rate:      accretion rate in solar mass per year (converted in g per second)
+#         -dist:          distance to the central core in the spherical coordinates (dist, theta, phi) [au]
+#         -R_centri:      centrifugal radius [au]
+#         -mu:            cos(theta) where theta is the angle above the midplane
+#         -mu0:           initial streamline of the infalling material
+#     """	
+#     def env_density(self, acc_rate, star_mass, dist, R_centri, mu, mu_0):
+#         return (((acc_rate*M_sun)/3.154e7)/(4*np.pi))*(Ggram*star_mass*M_sun*(dist*autocm)**3)**(-1/2)*(1 + mu/mu_0)**(-1/2)*(mu/mu_0 + 2*mu_0**2*(R_centri/dist))**(-1)
 
-    # #class Dust:
-    #     """
-    #     This Class gives the physical quantities related to the grains in the envelope.
-    #     Equation references are those of the documentation.
-    #     Instance methods:
-	#         A) initializer ...
-    #     """
+#     # #class Dust:
+#     #     """
+#     #     This Class gives the physical quantities related to the grains in the envelope.
+#     #     Equation references are those of the documentation.
+#     #     Instance methods:
+# 	#         A) initializer ...
+#     #     """
         
--- example_simulation/example_simulation.py
+++ example_simulation/example_simulation.py
@@ -2,16 +2,17 @@ import chemdiskpy.modeling as modeling
 import chemdiskpy.dust as dust
 import chemdiskpy.plotting.plot as plot
 
-m = modeling.YSOModel()  
+m = modeling.Structure()  
 
 
 #-----DISK PARAMETERS
 nr, ntheta, nphi = 301, 181, 2
-rin, rout= 1, 500
+rin, rout= 2, 500
 dtogas = 1e-2
 star_mass = 1.
-dust_mass = 1e-4
+dust_mass = 7.5e-5
 q_c = 12
+q_exp = 0.4
 
 #-----CREATE WAVELENGTH TABLE----
 m.grid.set_wavelength_grid(1e-1, 2e3, 100, log=True) #microns
@@ -22,21 +23,24 @@ m.grid.set_mcmonowavelength_grid(1e-1, 2e3, 100, log=True) #microns
 m.set_spherical_grid(rin, rout, nr, ntheta, nphi, log=True)
 
 # #-----CREATE STARS-----
-m.add_star(mass=star_mass, luminosity=0.8, temperature=3900., x=0., y=0., z=0.)
+m.add_star(mass=star_mass, luminosity=1.5, temperature=4100., x=0., y=0., z=0.)
 
 # # #-----CREATE ISRF-----
 m.add_isrf(cut=2.e-1, d78=True, vdb82=True)
 
-# -----CREATE DUST DENSITIES-----
+# # -----CREATE DUST DENSITIES-----
 d = dust.DustDistrib(rsingle=0.1, amin=5.000e-03, amax=1.000e+03, nb_sizes=2, rho_m=3.) #microns
+frac = d.massfraction()
 sizes = d.sizes()
 mass = d.grainmass()
 print(sizes[-1])
-m.add_disk(dust=d, rin=rin, rout=rout, star_mass=star_mass, dtogas=dtogas, dust_mass=dust_mass, q_c=q_c, settling=True, coordsystem='spherical')
+print(frac)
+print(mass)
+m.add_disk(dust=d, rin=rin, rout=rout, Tmidplan_ref=15, star_mass=star_mass, q_exp= q_exp, dtogas=dtogas, dust_mass=dust_mass, q_c=q_c, settling=True, coordsystem='spherical')
 
 # -----RUN THERMAL-----
-m.thermal(run=True, nphot = 2e7, \
-              nphot_scat = 2e7, \
+m.thermal(run=True, nphot = 1e7, \
+              nphot_scat = 1e7, \
               nphot_spec = 1e5, \
               itempdecoup = 1, \
               istar_sphere = 1, \
@@ -46,14 +50,19 @@ m.thermal(run=True, nphot = 2e7, \
               tgas_eq_tdust = 0)
 
 
-m.localfield(run=True, nphot_mono = 2000000)
+m.localfield(run=False, nphot_mono = 2000000)
 
-rchem = [10, 20, 30, 50, 80, 100, 140, 160, 200]
+rchem = [50,55,60,100,110,120,130,140,150,160]
 m.grid.set_nautilus_grid(rchem, max_h=4, nz_chem=64)
 
-m.add_nautilusdisk(dust=d, rin=rin, rout=rout,  star_mass=star_mass, dust_mass=dust_mass, q_c=q_c, dtogas=dtogas, settling=True)
+m.add_nautilusdisk(dust=d, rin=rin, rout=rout,  Tmidplan_ref = 15, star_mass=star_mass, dust_mass=dust_mass, q_c=q_c, q_exp= q_exp, dtogas=dtogas, settling=True)
 
-m.write_nautilus(sizes=sizes, uv_ref=3400, dtogas=dtogas, ref_radius=m.disk.ref_radius, cr_ionisation_rate=1.300E-17, network=False, coupling_av=False)
+m.write_nautilus(nmgc=True, sizes=sizes, uv_ref=3400, dtogas=dtogas, \
+                 ref_radius=m.disk.ref_radius, is_h2_adhoc_form = 0, is_h2_formation_rate=1, \
+                 height_h2formation=64, is_photodesorb = 1, cr_ionisation_rate=1.300E-17, \
+                 stop_time=2e6, nb_outputs = 60, network=False, coupling_av=False)
 
 plot.vertical_temp(r=100)
+plot.density2D(d.grainmass())
 plot.midplane_temp()
+plot.temperature2D()
